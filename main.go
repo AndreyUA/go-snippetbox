493782f376c95c70e3a79008ee40b1035844f23e
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-07-17 21:22:16 +0300

    new routes with servemux

--- main.go
+++ main.go
@@ -6,15 +6,32 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	w.Write([]byte("Home route."))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display snippet."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create new snippet."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
+	//Routes
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Server started on http://localhost:4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Server started on http://localhost:4200")
+	err := http.ListenAndServe(":4200", mux)
 	log.Fatal(err)
 }
