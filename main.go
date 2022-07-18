bac97fb58f287e02122540be686b6ade141f0b88
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-07-18 19:32:05 +0300

    get snippet from query param

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display snippet."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	fmt.Fprintf(w, "Snippet number %d", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
