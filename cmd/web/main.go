2324858812ea55ecfa41e54bd30fc15c5ffbabd7
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-07-19 12:15:12 +0300

    static files and Open method for handling them

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -13,7 +14,43 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Static files
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Server started on http://localhost:4200")
 	err := http.ListenAndServe(":4200", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+// Open prevents to show static files
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
