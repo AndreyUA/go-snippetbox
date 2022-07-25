a288598dbba8d3f415f7950ff24bd00d161013b2
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-07-25 13:42:00 +0300

    pull of connections to MySQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,12 +19,22 @@ type application struct {
 func main() {
 	// Get port number from environment
 	addr := flag.String("addr", ":4200", "network address")
+	// Get MySQL options from environment
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL database credentials")
 	flag.Parse()
 
 	// Create info logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	// Create error logger
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -37,8 +50,22 @@ func main() {
 
 	// Use info logs
 	infoLog.Printf("Server started on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 type neuteredFileSystem struct {
